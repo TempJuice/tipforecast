0c1a0623761606da0c04fa6fa6d1dfc64ff9b727
Ryan Templin <tempjuice@hotmail.com>
2017-09-09 13:44:07 -0500

    Transaction items are building correctly and storing all properties in DB correctly.

--- server/routes/info.router.js
+++ server/routes/info.router.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../modules/pool')
 
 router.post('/', function (req, res) {
     console.log('Info post route was hit server side :', req.body);
 
     var incomeItem = {
         username: req.body.userName,
-        date: req.body.date,
         type: req.body.type,
+        date: req.body.date,
+        description: req.body.description,
         amount: req.body.amount
     };
 
@@ -16,8 +18,8 @@ router.post('/', function (req, res) {
             console.log("Error connecting: ", err);
             res.sendStatus(500);
         }
-        client.query("UPDATE users (date, type, amount) VALUES ($1, $2, $3) WHERE ",
-            [saveUser.username, saveUser.password],
+        client.query("INSERT INTO transactions (username, type, date, description, amount)  VALUES ($1, $2, $3, $4, $5);",
+            [incomeItem.username, incomeItem.type, incomeItem.date, incomeItem.description, incomeItem.amount],
             function (err, result) {
                 client.end();
 
--- tipforecast.sql
+++ tipforecast.sql
@@ -1,13 +1,24 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(25) NOT NULL,
-    password VARCHAR(80) NOT NULL,
+    password VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE transactions (
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(25),
+	type VARCHAR(12),
 	date INT,
-	type VARCHAR (25),
+	description VARCHAR (40),
 	amount INT
 );
 
-DROP TABLE USERS;
+
+DROP TABLE transactions;
+
+INSERT INTO transactions (username, type, date, description, amount)
+VALUES ($1, $2, $3, $4, $5);
+
 
 ALTER TABLE users
 ADD amount INT;
@@ -15,7 +26,7 @@ ADD amount INT;
 ALTER TABLE users
 DROP COLUMN amount;
 
-INSERT INTO users (date, type, amount) 
-VALUES ($1, $2, $3) 
-WHERE 
+UPDATE users
+SET date = 111, type = 'aaa', amount = 222
+WHERE username = ryan;
 
