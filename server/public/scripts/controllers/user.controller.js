9f0bf27377fb009b4d4544912ae1c5c5e448fb44
Ryan Templin <tempjuice@hotmail.com>
2017-09-22 03:36:45 -0500

    Moved math and calendar functions from service to controller so object would not stack values.

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,82 +1,79 @@
-myApp.controller('UserController', function (UserService, TransactionService) {
+myApp.controller('UserController', function (UserService, TransactionService, $http) {
   console.log('UserController created');
   var vm = this;
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
 
+  //Global object to store calendar data
+  vm.domCalendarObject = { list: [] }
 
-  // TransactionService.getTransactions();
-  // TransactionService.changeObjectsForCalendar();
-  TransactionService.getMath();
-  // TransactionService.getCalendar();
+  // vm.mathObject = TransactionService.mathObject;
+  vm.mathObject = {
+    incomeTotal: 0,
+    expTotal: 0,
+    total: 0
+  };
 
+  //function that formats db objects so calendar can populate
+  vm.getCalendar = function () {
+    // console.log('Get math function was triggered');
+    $http.get('/info/calendar').then(function (response) {
+      console.log('Calendar get route brought back: ', response.data);
+      for (var i = 0; i < response.data.length; i++) {
+        var event = response.data[i];
+        dateObject = {};
+        dateObject.title = event.description;
+        dateObject.start = event.date;
+        vm.domCalendarObject.list.push(dateObject);
+      }
+      console.log('domObject is now: ', vm.domCalendarObject.list);
+    })
+  };
+  vm.getCalendar();
 
-  vm.mathObject = TransactionService.mathObject;
-
-  vm.transactionArray = TransactionService.transactionsObject
-
-  vm.dateArray = TransactionService.domCalendarObject.list;
-  // console.log( 'Array coming into controller: ', vm.dateArray);
-
-  // objects come in like: {
-  //   username: 'ryan',
-  //   type: 'expenditure',
-  //   date: '09/21/2017',
-  //   description: 'Car payment',
-  //   amount: 100
-  // }
-
-
-
-
-  // vm.transactionObjects = function () {
-  //   console.log('this is something', TransactionService.transactionsObject);
-  //   for (var i = 0; i < vm.transactionArray.list.length; i++) {
-  //     var event = vm.transactionArray.list[i];
-  //     console.log('this is the event', event);
-
-  //     // event.title = transactionArray.list.description
-
-  //   }
-
-  // }
-
-  // vm.transactionObjects();
+  vm.dateArray = vm.domCalendarObject.list;
 
 
 
 
-
+  // Sample moment.js commands
   // var date = new Date();
   // var d = date.getDate();
   // var m = date.getMonth();
   // var y = date.getFullYear();
 
-  // vm.eventSources = {
-  //   events: [
-  //     { title: 'Big Event', start: new Date(y, m, 1) },
-
-  //   ]
-  // };
-
 
   vm.eventSources = [{
     events: vm.dateArray
   }];
-  // console.log('Array after being applied to calendar source: ', vm.dateArray);
 
-
-  // vm.eventSources = {
-  //   events: [{"title":"bar","start":"2017-09-19T05:00:00.000Z"},{"title":"serve","start":"2017-09-21T05:00:00.000Z"},{"title":"bar","start":"2017-09-26T05:00:00.000Z"},{"title":"Car payment","start":"2017-09-23T05:00:00.000Z"}]
-  // };
-
-
+  //Calendar input examples
   // { title: 'All Day Event', start: new Date(y, m, 1) },
   // { title: 'Long Event', start: new Date(y, m, d - 5), end: new Date(y, m, d - 2) },
   // { id: 999, title: 'Repeating Event', start: new Date(y, m, d - 3, 16, 0), allDay: false },
   // { id: 999, title: 'Repeating Event', start: new Date(y, m, d + 4, 16, 0), allDay: false },
   // { title: 'Birthday Party', start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false },
   // { title: 'Click for Google', start: new Date(y, m, 28), end: new Date(y, m, 29), url: 'http://google.com/' }
+
+  vm.getMath = function () {
+    // console.log('Get math function was triggered');
+    $http.get('/info/math').then(function (response) {
+      // console.log('Math get route brought back: ', response.data);
+      for (var i = 0; i < response.data.length; i++) {
+        var math = response.data[i];
+        if (math.type === 'income') {
+          vm.mathObject.incomeTotal += math.amount
+        } else {
+          vm.mathObject.expTotal -= math.amount
+        }
+        // console.log('mathObject is now: ', vm.mathObject);                
+      }
+      vm.mathObject.total = vm.mathObject.incomeTotal + vm.mathObject.expTotal;
+      // console.log('mathObject.total is: ', vm.mathObject.total);
+    })
+  };
+  //Calls function that calculates inc/exp totals for user
+  vm.getMath();
 
 
 });
--- server/public/scripts/services/transaction.service.js
+++ server/public/scripts/services/transaction.service.js
@@ -3,12 +3,8 @@ myApp.service('TransactionService', function ($http) {
 
     self.transactionsObject = { list: [] };
     self.calendarObjects = {};
-    self.domCalendarObject = { list: [] }
-    self.mathObject = {
-        incomeTotal: 0,
-        expTotal: 0,
-        total: 0
-    };
+    // self.domCalendarObject = { list: [] }
+    
 
 
 
@@ -21,75 +17,45 @@ myApp.service('TransactionService', function ($http) {
         $http.get('/info').then(function (response) {
             console.log('Transactions get route brought back: ', response.data);
             // console.log('date format is: ', response.data);
-            // self.calendarObjects = response.data;
             self.transactionsObject.list = response.data;
-            // self.changeObjectsForCalendar();
-            // for (var i = 0; i < response.data.length; i++) {
-            //     var event = response.data[i];
-            //     // console.log('this is the event', event);
-            //     dateObject = {};
-            //     dateObject.id = event.id;
-            //     dateObject.username = event.username;
-            //     dateObject.type = event.type;
-            //     dateObject.date = new Date(event.date);
-            //     dateObject.description = event.description;
-            //     self.transactionsObject.list.push(dateObject);
-            // //     // dateObject.title = event.description;
-            // //     // dateObject.start = dateObject.date;
-            // //     // self.domCalendarObject.list.push(dateObject);
-            // }
-            // console.log('Transactions Object: ', self.transactionsObject.list);
-            // self.changeObjectsForCalendar();
         })
     };//end of getTransactions function
 
 
-    // self.changeObjectsForCalendar = function () {
-    //     // console.log('calendar objects are: ', self.calendarObjects);
-    //     for (var i = 0; i < self.calendarObjects.length; i++) {
-    //         var event = self.calendarObjects[i];
-    //         dateObject = {};
-    //         dateObject.title = event.description;
-    //         dateObject.start = event.date;
-    //         self.domCalendarObject.list.push(dateObject);
-    //     }
-    // }
-
-
-    self.getMath = function () {
-        // console.log('Get math function was triggered');
-        $http.get('/info/math').then(function (response) {
-            // console.log('Math get route brought back: ', response.data);
-            for (var i = 0; i < response.data.length; i++) {
-                var math = response.data[i];
-                if (math.type === 'income') {
-                    self.mathObject.incomeTotal += math.amount
-                } else {
-                    self.mathObject.expTotal -= math.amount
-                } 
-                // console.log('mathObject is now: ', self.mathObject);                
-            }
-            self.mathObject.total = self.mathObject.incomeTotal + self.mathObject.expTotal;
-            // console.log('mathObject.total is: ', self.mathObject.total);
+    // self.getMath = function () {
+    //     // console.log('Get math function was triggered');
+    //     $http.get('/info/math').then(function (response) {
+    //         // console.log('Math get route brought back: ', response.data);
+    //         for (var i = 0; i < response.data.length; i++) {
+    //             var math = response.data[i];
+    //             if (math.type === 'income') {
+    //                 self.mathObject.incomeTotal += math.amount
+    //             } else {
+    //                 self.mathObject.expTotal -= math.amount
+    //             } 
+    //             // console.log('mathObject is now: ', self.mathObject);                
+    //         }
+    //         self.mathObject.total = self.mathObject.incomeTotal + self.mathObject.expTotal;
+    //         // console.log('mathObject.total is: ', self.mathObject.total);
             
-        })
-    };
+    //     })
+    // };
 
-    self.getCalendar = function () {
-        // console.log('Get math function was triggered');
-        $http.get('/info/calendar').then(function (response) {
-            console.log('Calendar get route brought back: ', response.data);
-            for (var i = 0; i < response.data.length; i++) {
-                var event = response.data[i];
-                dateObject = {};
-                dateObject.title = event.description;
-                dateObject.start = event.date;
-                self.domCalendarObject.list.push(dateObject);  
-            }
-            console.log('domObject is now: ', self.domCalendarObject.list);
-        })
-    };
-    self.getCalendar();
+    // self.getCalendar = function () {
+    //     // console.log('Get math function was triggered');
+    //     $http.get('/info/calendar').then(function (response) {
+    //         console.log('Calendar get route brought back: ', response.data);
+    //         for (var i = 0; i < response.data.length; i++) {
+    //             var event = response.data[i];
+    //             dateObject = {};
+    //             dateObject.title = event.description;
+    //             dateObject.start = event.date;
+    //             self.domCalendarObject.list.push(dateObject);  
+    //         }
+    //         console.log('domObject is now: ', self.domCalendarObject.list);
+    //     })
+    // };
+    // self.getCalendar();
 
     self.addTransaction = function (newTransaction) {
         console.log('add item function was initiated');
